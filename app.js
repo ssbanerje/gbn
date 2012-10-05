afb511c878831158855cccae871a1f2996dfcf5e
Subho Banerjee <subs.zero@gmail.com>
2012-10-06 00:37:51 +0530

    express3 running

--- app.js
+++ app.js
@@ -10,6 +10,7 @@
 var program = require('commander');
 var fs = require('fs');
 var child_process = require('child_process');
+var database = require('./server/database.js');
 
 function openFile(file) {
 	var browser;
@@ -32,10 +33,10 @@ program
 program
 	.command('check-db')
 	.description('Check to see if the database is working correctly')
-	.action(function(){
+	.action(function () {
     var path = require('path');
 		var manifestFile = __dirname+'/raw-data/manifest.json';
-		path.exists(manifestFile, function(exists) {
+		path.exists(manifestFile, function (exists) {
 		if(exists) {
 			console.log(JSON.parse(fs.readFileSync(manifestFile, 'utf-8')));
 			console.log('Database has been and verified!');
@@ -48,8 +49,7 @@ program
 program
 	.command('query <JSON_query>')
 	.description('Query the system without starting a server. Each query must be in JSON format')
-	.action(function(string) {
-		var database = require('./server/database.js');
+	.action(function (string) {
 		var i, results = database.queryDB(string);
 		console.log(JSON.stringify(results, null, '\t'));
 		for(i in results) {
@@ -68,10 +68,54 @@ program
 program
 	.command('server')
 	.description('Start server on 8080')
-	.action(function() {
-		var server = require('./server/server.js');
-		server.createServer();
-		openFile('http://localhost:8080');
+	.action(function () {
+		var express = require('express');
+		var http = require('http');
+		var path = require('path');
+		var app = express();
+
+		app.configure(function () {
+			app.set('port', process.env.app_port || 3000);
+			app.set('views', __dirname + '/views');
+			app.set('view engine', 'ejs');
+			app.use(express.favicon());
+			app.use(express.logger('dev'));
+			app.use(express.bodyParser());
+			app.use(express.methodOverride());
+			app.use(express.cookieParser('onlinegitobitan@gbn'));
+			app.use(express.session());
+			app.use(app.router);
+			app.use(express.static(path.join(__dirname, 'public')));
+			app.use(function (req, res, next) {
+				if (req.accepts('html')) {
+					res.status(404).render('404', { url: req.url });
+					return;
+				}
+				if (req.accepts('json')) {
+					res.send(404, { error: 'Not found' });
+					return;
+				}
+				res.type('txt').send(404, 'Not found');
+			});
+		});
+		app.configure('development', function () {
+			app.use(express.errorHandler());
+		});
+		var server = http.createServer(app).listen(app.get('port'), function () {
+			console.log("Express HTTP server listening on port " + app.get('port'));
+		});
+
+		var io = require('socket.io').listen(server);
+		io.set('log level', 2);
+		setInterval(database.updateManifest, 600000);
+
+		io.sockets.on('connection', function(socket){
+			socket.on('query', function(data){
+				var results = database.query(data);
+				socket.emit('queryResponse', results);
+			});
+		});
+		openFile('http://localhost:'+app.get('port'));
 	});
 
 // Lets get started
