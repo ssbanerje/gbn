acd60f05df7bf97f1e0ec9178d38b5c3eba2c440
Subho Banerjee <subs.zero@gmail.com>
2012-10-10 13:44:18 +0530

    partially moving to angularjs - build fails

--- public/js/page.js
+++ public/js/page.js
@@ -1,42 +1,3 @@
-// Store all the results of the page
-var results = null;
-
-// The object to control page behavior
-var Page = {
-	showResults: function (idx) {
-		spinner.spin(spinnerSpan);
-		$('#details').html('<ul><li><b>Name:</b> '+results[idx]['name']+'</li>'
-							+'<li><b>Taal:</b> '+results[idx].taal+'</li>'
-							+'<li><b>Raag:</b> '+results[idx].raag+'</li>'
-							+'<li><b>Parjaay:</b> '+results[idx].parjaay+'</li></ul>');
-		$.get(results[idx]['lyrics'], function(data) {
-				$('#lyrics').html(data);
-		});
-		$('#notation').html('<img src="/'+results[idx].notation+'" width="100%" alt="Lyrics..."/>');
-		$('#songs').html('<iframe width="100%" height="300px"'
-							+' src="http://www.youtube.com/embed/?listType=search&list='+results[idx].name.replace(/ /g,'+')
-							+'+tagore+bengali&showinfo=1" frameborder="0" allowfullscreen></iframe>');
-		$('#song-details').show('fast');
-		spinner.stop();
-	},
-
-	toggleResults: function () {
-	  $('#result-list').slideToggle();
-	},
-
-	clearResults: function () {
-		$('#result-list').slideUp();
-		$('#footer').hide();
-		spinner.stop();
-		clearSongDetails();
-	},
-	
-	clearSongDetails: function () {
-		$('#song-details').hide('fast');
-		$('#songs').html('');
-	}
-};
-
 // Setup the busy spinner
 var opts = {
 	lines: 7, // The number of lines to draw
@@ -58,68 +19,132 @@ var opts = {
 var spinnerSpan = document.getElementById('spinner');
 var spinner = new Spinner(opts);
 
+// The AngularJS module
+var AngApp = angular.module('gbn', ['ui']);
 
-// Main() --- On load
-$(function () {
+// Socket.io for all controllers
+AngApp.factory('socket', function ($rootScope) {
 	var socket = io.connect(window.location.origin);
-	
-	// Listen for keypresses in the search field
-	$('#search-field').keypress(function (e) {
-		if (e.which == 13) {
-			var jsonQuery = null;
-			try {
-				jsonQuery = JSON.parse(this.value)
-			} catch (err) {
-				alert(err);
-			}
-			if(jsonQuery != null) {
-				socket.emit('query', jsonQuery);
-				spinner.spin(spinnerSpan);
-			}
+	return {
+		on: function (eventName, callback) {
+			socket.on(eventName, function () {
+				var args = arguments;
+				$rootScope.$apply(function () {
+					callback.apply(socket, args);
+				});
+			});
+		},
+		emit: function (eventName, data, callback) {
+			socket.emit(eventName, data, function () {
+				var args = arguments;
+				$rootScope.$apply(function () {
+					if (callback) {
+						callback.apply(socket, args);
+					}
+				});
+			});
 		}
-	});
-	
-	// Clear result click listeners
-	$('#main-heading').click(Page.clearResults);
-	$('#clear-results').click(Page.clearResults);
-	
-	// Toggle results listener
-	$('#toggle-button').click(Page.toggleResults);
-	
-	// Click handlers for documentation links
-	$('#disclaimer').click(function () {
-		$('#disclaimer-message').fadeIn(200);
-	});	
-	$('#qquery').click(function () {
-		$('#qualified-message').fadeIn(200);
-	});	
-	$('.use-query').click(function () {
-		$('#search-field').val(this.getAttribute('data'));
-		$('.overlay').fadeOut(200);
-	});	
-	$('.close').click(function () {
-		$('.overlay').fadeOut(200);
-	});
+	};
+});
+
+// The Controller
+var Songs = function($scope, socket) {
+	// $scope Methods and Variables
+	$scope.queryText = '{"name": "abc"}'; // Text of the Query
+	$scope.song = { // The song which is being displayed
+		idx : 0,
+		lyrics : ''
+	};
+	$scope.results = [ // Results of running the Query
+		{
+			taal: '',
+			date: '',
+			name: '',
+			raag:'',
+			parjaay: '',
+			notation: '',
+			lyrics: ''
+		}
+	];
+	$scope.makeQuery = function () { // Make the query on server with Socket.io
+		var query = null;
+		try {
+			query = JSON.parse($scope.queryText)
+		} catch (err) {
+			alert(err);
+		}
+		if(query != null) {
+			socket.emit('query', query);
+			spinner.spin(spinnerSpan);
+		}
+	}
+	$scope.getDetails = function(idx) { // Get the lyrics of the requested song
+		spinner.spin(spinnerSpan);
+		$scope.song.idx = idx;
+		$.get($scope.results[idx].lyrics, function(data) {
+				$scope.song.lyrics = data;
+		});
+		$('#song-details').show('fast');
+		spinner.stop();
+	}
+	$scope.foo = function() {	// Test
+		alert('test');
+	}
 	
 	// Handle Socket.io communications
 	socket.on('queryResponse', function (data){
-		$('#query-nos').html('('+data.length+')');
-		if(data.length == 0) {
-			$('#footer').show()
-			$('#result-list').slideUp()
-			$('#result-list').html('');
-		}
+		$scope.results = data;
 		Page.clearSongDetails();
-		var htmlString = '';
-		results = data;
-		for(var i in data) {
-				htmlString = htmlString + '<li value="'+i+'"onclick="Page.showResults(this.value)">'+data[i]['name']+'</li>';
-		}
-		$('#result-list').html(htmlString);
 		$('#footer').show()
 		$('#result-list').slideDown()
 		spinner.stop()
 	});
-
 	
+	// Private Methods
+};
+
+
+
+
+// The object to control page behavior
+var Page = {
+	toggleResults: function () {
+	  $('#result-list').slideToggle();
+	},
+
+	clearResults: function () {
+		$('#result-list').slideUp();
+		$('#footer').hide();
+		spinner.stop();
+		Page.clearSongDetails();
+	},
+
+	clearSongDetails: function () {
+		$('#song-details').hide('fast');
+	}
+};
+
+// Main() --- On load
+$(function () {
+	// Clear result click listeners
+	$('#main-heading').click(Page.clearResults);
+	$('#clear-results').click(Page.clearResults);
+
+	// Toggle results listener
+	$('#toggle-button').click(Page.toggleResults);
+
+	// Click handlers for documentation links
+	$('#disclaimer').click(function () {
+		$('#disclaimer-message').fadeIn(200);
+	});
+	$('#qquery').click(function () {
+		$('#qualified-message').fadeIn(200);
+	});
+	$('.use-query').click(function () {
+		$('#search-field').val(this.getAttribute('data'));
+		$('.overlay').fadeOut(200);
+	});
+	$('.close').click(function () {
+		$('.overlay').fadeOut(200);
+	});
 });
